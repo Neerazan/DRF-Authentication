929b39ce4709ce1036d29bf9aa064513acdfd061
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-02-20 22:17:16 +0545

    Conflict in Token class due to custome user model will be fixed in future

--- playground/models.py
+++ playground/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.contrib.auth.models import AbstractUser
+from rest_framework.authtoken.models import Token
+from django.conf import settings
 
 
 class User(AbstractUser):
@@ -8,6 +10,12 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'username'
+
+
+
+class Token(Token):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
