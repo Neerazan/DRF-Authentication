cf642c99d792b2811ed03bbbaa42a212688c8254
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-02-20 12:39:28 +0545

    Models createad

---
+++ playground/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import AbstractBaseUser
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits = 6,
+        decimal_places = 2,
+        validators = [MinValueValidator(1)]
+    )
+    last_updated = models.DateTimeField(auto_now=True)
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
